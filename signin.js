b531b27dd7b6a69c6e3406c11e9e911895efa058
Santiago199723 <71418175+Santiago199723@users.noreply.github.com>
2024-08-02 16:40:33 -0300

    signin.js

--- signin.js
+++ signin.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(() => {
         console.log("Login bem-sucedido.");
         hideLoading();
-        window.location.href = "tela.html";
+        window.location.href = "indicador.html";
       })
       .catch(error => {
         console.error("Erro durante o login:", error);
